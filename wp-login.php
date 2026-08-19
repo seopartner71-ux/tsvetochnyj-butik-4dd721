c1f073f7c7d68d22f48d7817ced0d3c0b818d158
Vladimir Sinitsyn <seo.partner71@gmail.com>
2026-08-19 12:30:01 +0300

    Update generated site for tsvetochnyj-butik-4dd721.vercel.app

--- wp-login.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html><head>
-<meta charset="UTF-8">
-<meta http-equiv="refresh" content="0; url=https://tsvetochnyj-butik-4dd721.vercel.app/">
-<link rel="canonical" href="https://tsvetochnyj-butik-4dd721.vercel.app/">
-<title>Redirecting…</title>
-</head>
-<body>Redirecting to <a href="https://tsvetochnyj-butik-4dd721.vercel.app/">https://tsvetochnyj-butik-4dd721.vercel.app/</a>.</body>
-</html>
